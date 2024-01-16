258f1c9b3d5719285919d0786c82dd90b7cc9a14
cosmos <va-litv@yandex.ru>
2024-01-16 18:30:58 +0300

    copy 007-middleware

---
+++ 008-ejs/store.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class Book {
+  constructor(title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "", fileBook = "", id = uuidv4()) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.authors = authors;
+      this.favorite = favorite;
+      this.fileCover = fileCover;
+      this.fileName = fileName;
+      this.fileBook = fileBook;
+  }
+}
+
+export const store = {
+  books: [
+    new Book(
+      'Человек-невидимка',
+      'В этом романе описывается судьба английского учёного-медика, а впоследствии физика Гриффина, который изобрёл аппарат, делающий человека невидимым',
+      'Герберта Уэллс',
+      'true',
+      'fileCover.jpg',
+      'fileName.pdf',
+      'public/books/the-invisible-man.jpeg',
+    ),
+  ],
+};

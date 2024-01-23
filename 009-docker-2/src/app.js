73deb060ad4d31ab5bb0d6adeac58763bcde6672
cosmos <va-litv@yandex.ru>
2024-01-23 17:22:58 +0300

    add src folder

--- 009-docker-2/src/app.js
+++ 009-docker-2/src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 import userRouter from './routes/user.js';
 import booksRouter from './routes/books.js';
 import apiBooksRouter from './routes/api-books.js';
 import { error404, error400 } from './middleware/error.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.use('/api/user', userRouter);
 app.use('/api/books', apiBooksRouter);

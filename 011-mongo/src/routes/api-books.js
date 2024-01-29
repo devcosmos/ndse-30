bc21b6dcb671dfa7a6d56bebf838e8dbbcf7a209
cosmos <va-litv@yandex.ru>
2024-01-29 18:57:57 +0300

    third task: update api

--- 011-mongo/src/routes/api-books.js
+++ 011-mongo/src/routes/api-books.js
@@ -1,104 +1,83 @@
 import { Router } from 'express';
-import { store, Book } from '../store.js';
+import { Book } from '../models/books.js';
 import fileMulter from '../middleware/fileMulter.js';
 
 const apiBooksRouter = Router();
 
 // получить все книги
-apiBooksRouter.get('/', (req, res) => {
-  const { books } = store;
-  res.json(books);
+apiBooksRouter.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    
+    res.json(books);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+  
 });
 
 // получить книгу по ID
-apiBooksRouter.get('/:id', (req, res) => {
-  const { books } = store;
+apiBooksRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const idx = books.findIndex(el => el.id === id);
+  try {
+    const book = await Book.findById(id);
 
-  if (idx !== -1) {
-    fetch(`${process.env.COUNTER_URL}/counter/${id}/incr`, { method: 'POST' })
-      .then(response => response.text())
-      .then(result => {
-        books[idx].count = result;
-        
-        res.json(books[idx]);
-      }).catch(() => {
-        res.status(500);
-        res.json('500 | Внутренняя ошибка сервера');
-      });
-  } else {
-    res.status(404);
-    res.json('404 | Книга не найдена');
+    res.json(book);
+  } catch (e) {
+    res.status(500).json(e);
   }
 });
 
 // создать книгу
-apiBooksRouter.post('/', fileMulter.single('fileBook'), (req, res) => { 
+apiBooksRouter.post('/', fileMulter.single('fileBook'), async (req, res) => { 
   if (req.file) {
-    const { books } = store;
     const { title, description, authors, favorite, fileCover, fileName } = req.body;
     const { path } = req.file;
-  
-    const newBook = new Book(title, description, authors, favorite, fileCover, fileName, path);
-    books.push(newBook);
-  
-    res.status(201);
-    res.json(newBook);
+
+    const newBook = new Book({ title, description, authors, favorite, fileCover, fileName, fileBook: path });
+    
+    try {
+      await newBook.save();
+      res.json(newBook);
+    } catch (e) {
+      res.status(500).json(e);
+    }
   } else {
-    res.status(400);
-    res.json('400 | Некорректный запрос');
+    res.status(400).json('400 | Некорректный запрос');
   }
 });
 
-// 	редактировать книгу по ID
-apiBooksRouter.put('/:id', (req, res) => {
-  const { books } = store;
+//	редактировать книгу по ID
+apiBooksRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { title, description, authors, favorite, fileCover, fileName } = req.body;
+  
+  const newBook = { title, description, authors, favorite, fileCover, fileName };
+  
+  if (req.file) {
+    const { path } = req.file;
+    newBook.fileBook = path;
+  }
 
-  const idx = books.findIndex(el => el.id === id);
-
-  if (idx !== -1) {
-    books[idx] = {
-      ...books[idx], title, description, authors, favorite, fileCover, fileName
-    }
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json('404 | Книга не найдена');
+  try {
+    await Book.findByIdAndUpdate(id, newBook);
+    res.redirect(`/api/books/${id}`);
+  } catch (e) {
+    res.status(500).json(e);
   }
 });
 
 // удалить книгу по ID
-apiBooksRouter.delete('/:id', (req, res) => {
-  const { books } = store;
+apiBooksRouter.delete('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const idx = books.findIndex(el => el.id === id);
+  try {
+    await Book.deleteOne({_id: id });
 
-  if (idx !== -1) {
-    books.splice(idx, 1);
     res.json('ok');
-  } else {
-    res.status(404);
-    res.json('404 | Книга не найдена');
-  }
-});
-
-// получить файл книги по ID
-apiBooksRouter.get('/:id/download', (req, res) => {
-  const { books } = store;
-  const { id } = req.params;
-
-  const idx = books.findIndex(el => el.id === id);
-
-  if (idx !== -1) {
-    res.download(books[idx].fileBook);
-  } else {
-    res.status(404);
-    res.json('404 | Книга не найдена');
+  } catch (e) {
+    res.status(500).json(e);
   }
 });
 

f03367d5f2decd80c10d41b0c0292c4aeb8ea732
devcosmos <va-litv@yandex.ru>
2024-03-22 18:03:49 +0300

    feat: create book interface and abstract class BooksRepository

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,1 +1,33 @@
-console.log('adasda');
+interface Book {
+  id: number,
+  title: string,
+  description: string,
+  authors: string,
+  favorite: boolean,
+  fileCover: string,
+  fileName: string,
+  fileBook: string,
+  count: number,
+}
+
+abstract class BooksRepository {
+  createBook(book: Book): void { // создание книги.
+    console.log(`create ${book.title}`);
+  }
+
+  getBook(id: number): void { // получение книги по id.
+    console.log(`get book by ${id}`);
+  }
+
+  getBooks(): void { // получение всех книг.
+    console.log('get books');
+  }
+
+  updateBook(id: number): void { // обновление книги.
+    console.log(`update book by ${id}`);
+  }
+
+  deleteBook(id: number): void { // удаление книги.
+    console.log(`delete book by ${id}`);
+  }
+}
